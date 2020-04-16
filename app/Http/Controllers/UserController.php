0421af108edb44b7a94375bd658fbb86175f00af
zhangli <zhangli_ok@126.com>
2020-04-16 22:37:47 +0800

    基础页面

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,19 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-   public function create()
-   {			
-   		return view('users.create');
-   }
+    public function create()
+    {
+        return view('users.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
