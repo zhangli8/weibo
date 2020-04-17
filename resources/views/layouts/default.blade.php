64bf9b6a4db2656b3b6947e2abc32dfa7e9a4d07
zhangli <zhangli_ok@126.com>
2020-04-17 14:35:31 +0800

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
-  </head>
-  <body>
+</head>
+<body>
 
-    @include('layouts._header')
+@include('layouts._header')
 
-    <div class="container">
-      <div class="offset-md-1 col-md-10">
+<div class="container">
+    <div class="offset-md-1 col-md-10">
         @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
 
-      </div>
     </div>
-  </body>
+</div>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
 </html>
